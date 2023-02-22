57f1c62f336962f7648b0be62fd305c299e6ad75
unknown <nangmanh89@gmail.com>
2023-02-23 06:30:04 +0700

    first editing Sigup

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Background from "../assets/login.jpg";
+import styled from "styled-components";
 
 export default function BackgroundImage() {
-  return <div></div>;
+  return (
+    <Container>
+      <img src={Background} alt="background" />
+    </Container>
+  );
 }
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  img {
+    height: 100vh;
+    width: 100vw;
+  }
+`;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,11 +7,32 @@ function Signup() {
   return (
     <Container>
       <BackgroundImage />
-      jel
       <Header />
+      <div className="body flex column a-center j-center"></div>
+      <div className="text flex column"></div>
+      <h1>Unlimited movies, TV shows and more</h1>
+      <h4>Watch any where. Cancel anytime</h4>
+      <h6>Ready to watch? Enter your email to create or restart membership</h6>
+      <div>
+        <div className="form">
+          <input type="email" placeholder="Email Address" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Get Started</button>
+        </div>
+        <button>Log In</button>
+      </div>
     </Container>
   );
 }
 export default Signup;
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+  }
+`;
